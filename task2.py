be827fea4829c6b605d4edd227610ba0036dc01d
morkovkinn <bogdan.iktisanov@yandex.ru>
2024-11-25 21:29:48 +0300

    Update task2.py
    Изменил тип на список перед сортировкой

--- task2.py
+++ task2.py
@@ -1,7 +1,7 @@
 def find_common_participants(first, second, delim = ','):
     first_group = set(first.split(delim))
     second_group = set(second.split(delim))
-    return sorted(first_group.intersection(second_group))
+    return sorted(list(first_group.intersection(second_group)))
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
